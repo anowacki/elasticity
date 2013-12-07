a0c8be34dca15c432c5994c59dfef33ffaf40014
Andy Nowacki <andy.nowacki@bristol.ac.uk>
2013-12-07 11:12:25 -0800

    Add CIJ_sphere for sphere-type plots of phasevels
    This allows one to visualise a tensor as P velocity
    or S anisotropy+splitting from an arbitary viewpoint,
    with the option to add labels to specified directions.
    
    Also update CIJ_plot_cij2phasevels to output the required
    information for these plots.

--- CIJ_plot/src/CIJ_plot_cij2phasevels.f90
+++ CIJ_plot/src/CIJ_plot_cij2phasevels.f90
@@ -4,7 +4,7 @@ program CIJ_plot_cij2phasevels
 !  Helper application for the CIJ_plot shell script, which plots phase velocity
 !  surfaces for sets of elastic constants.
 !
-!  36 ecs and density read from stdin.  
+!  36 ecs and density read from stdin.
 !
 !  Finely-sampled points (for Vp and AVs contour plots) sent to stdout
 !  Coarser-sampled points (for Vs1 orientation) sent to stderr
@@ -16,42 +16,46 @@ program CIJ_plot_cij2phasevels
    use anisotropy_ajn
    use EmatrixUtils
    use spherical_geometry
-   
+
    implicit none
-   
+
    real(8) :: C(6,6), rho, inc, azi, pol, vp, vs1, vs2, avs, d
    real(8) :: minvp, maxvp, minavs, maxavs
    real(8),allocatable,dimension(:) :: lon, lat
    real(8) :: lo, la
-   character(len=1) :: arg
+   character(len=2) :: arg
    integer :: i,j,n
-   
-   interface 
+   integer :: iostat
+
+   interface
       subroutine sphere_sample_internal(lon,lat,n)
       implicit none
       real(8),intent(out),allocatable,dimension(:) :: lon,lat
       integer :: n
       end subroutine sphere_sample_internal
    end interface
-   
+
 !  Choose the output format we want:
    call getarg(1,arg)
-   
+
 !  Read density-normalised ecs and density from stdin
-   read(*,*) ((C(i,j),j=1,6),i=1,6),rho
-   
+   read(*,*,iostat=iostat) ((C(i,j),j=1,6),i=1,6),rho
+   if (iostat /= 0) then
+      write(0,'(a)') 'CIJ_plot_cij2phasevels: Error: Can''t read 36 ecs + rho from stdin'
+      stop
+   endif
+
 !###
 ! Vp output
 !###
-   
+
    if (arg == 'P') then
-   
+
 !  Get list of points to evaluate the velocities over
       d = 2.  ! Standard spacing of 5 degrees between points on the surface for the
               ! Vp and AVs plots
       call sphere_sample(d,lon,lat,n)
-!      call sphere_sample_internal(lon,lat,n)
-      
+
       minvp =  1.e36
       maxvp = -1.e36
 !  Calculate Vp at each of these points
@@ -69,14 +73,14 @@ program CIJ_plot_cij2phasevels
       enddo
       !  Write out min & max values
       write(*,*) ">",minvp,maxvp
-      
+
 !###
 ! AVs output
 !###
    else if (arg == 'S') then
       d = 2.
       call sphere_sample(d,lon,lat,n)
-!      call sphere_sample_internal(lon,lat,n)
+
       minavs =  1.e36
       maxavs = -1.e36
       do i=1,n
@@ -91,14 +95,14 @@ program CIJ_plot_cij2phasevels
          write(*,*) lo,la,avs
       enddo
       write(*,*) ">",minavs,maxavs
-      
+
 !###
 ! Fast orientation output
 !###
    else if (arg == 'F') then
       d=15.
       call sphere_sample(d,lon,lat,n)
-!      call sphere_sample_internal(lon,lat,n)
+
       do i=1,n
          if (lat(i) < 0.) exit
          inc = lat(i)
@@ -106,20 +110,70 @@ program CIJ_plot_cij2phasevels
          call CIJ_phasevels(C,rho,azi,inc,pol=pol)
          !  Orientation is given anticlockwise from x3 looking along ray.
          !  GMT wants them as azimuths from N, which is 1-axis.
-!         pol = pol + azi
          pol = 90. - pol - azi
          inc = 90. - lat(i)
          call incazi2lonlat(inc,azi,lo,la)
-!         if (lat(i) < 75. .or. mod(lon(i),30.) < 10.) &
-            write(*,*) lo,la,pol
+         write(*,*) lo,la,pol
       enddo
-      
+
+!###
+! Vp output on a sphere
+!###
+   else if (arg == 'PS') then
+            d = 2.
+            call sphere_sample(d,lon,lat,n)
+            minvp =  1.e36
+            maxvp = -1.e36
+            do i=1,n
+               inc = lat(i)
+               azi = -lon(i)
+               call CIJ_phasevels(C,rho,azi,inc,vp=vp)
+               if (vp < minvp) minvp = vp
+               if (vp > maxvp) maxvp = vp
+               write(*,*) lon(i),lat(i),vp
+            enddo
+            write(*,*) ">",minvp,maxvp
+
+!###
+! AVs output on a sphere
+!###
+   else if (arg == 'SS') then
+      d = 2.
+      call sphere_sample(d,lon,lat,n)
+      minavs =  1.e36
+      maxavs = -1.e36
+      do i=1,n
+         inc = lat(i)
+         azi = -lon(i)
+         call CIJ_phasevels(C,rho,azi,inc,avs=avs)
+         if (avs < minavs) minavs = avs
+         if (avs > maxavs) maxavs = avs
+         inc = 90. - lat(i)
+         write(*,*) lon(i),lat(i),avs
+      enddo
+      write(*,*) ">",minavs,maxavs
+
+!###
+! Fast orientation output on a sphere
+!###
+   else if (arg == "FS") then
+      d=15.
+      call sphere_sample(d,lon,lat,n)
+      do i=1,n
+         inc = lat(i)
+         azi = -lon(i)
+         call CIJ_phasevels(C,rho,azi,inc,pol=pol)
+         !  Orientation is given clockwise from x3 looking towards centre of sphere
+         !  GMT wants them as azimuths from N, which is the same thing
+         write(*,*) lon(i),lat(i),pol
+      enddo
+
 !  Unsupported command line argument
    else
       write(6,'(2a)') 'CIJ_plot_cij2phasevels: error: unrecognised option: ',arg
       call exit(1)
    endif
-   
+
 end program
 !_______________________________________________________________________________
 
@@ -133,276 +187,42 @@ subroutine incazi2lonlat(inc,azi,lon,lat)
 !  the x-axis (lon,lat=0,0).  Does these in global cartesians.
 
    use spherical_geometry
-   
+
    implicit none
-   
+
    real(8),intent(in)  :: inc,azi
    real(8),intent(out) :: lon,lat
    real(8) :: x,y,z,r, xr,yr,zr, t
    real(8),parameter :: pi = atan2(1.,1.)*4.
-   
+
 !   write(*,'(a,2f6.1)') 'inc,azi = ',inc,azi
-   
+
 !  Get coordinates for azi=0
    lat = inc
    lon = 0._8
    r = 1._8
-   
+
 !  Convert to Cartesian
 !   call geog2cart(lon,lat,r,x,y,z,degrees=.true.)
    x = cos(lat*pi/180._8)
    y = 0. ! Because we're always on the Greenwich meridian
    z = sin(lat*pi/180._8)
 !   write(*,*) 'lon,lat=',lon,lat,'  x,y,z=',x,y,z
-   
+
 !  Rotate to correct azimuth
    t = pi*azi/180.
    xr =  x
    yr =  y*cos(t) + z*sin(t)
    zr = -y*sin(t) + z*cos(t)
-   
+
 !   write(*,*) 'Rotated xyz =',xr,yr,zr
-   
+
 !  Convert back to geographic
 !   call cart2geog(x,y,z,lon,lat,r,degrees=.true.)
    lon = atan2(yr,xr)*180._8/pi
    lat = asin(zr)*180._8/pi
 !   write(*,*) 'Rotated lon,lat =',lon,lat
-   
+
    return
 end subroutine incazi2lonlat
-!-------------------------------------------------------------------------------
-
-!===============================================================================
-   subroutine sphere_sample_internal(lon,lat,n)
-!===============================================================================
-!  Internal version of sphere_sample which returns a hardwired set of lon,lat
-!  points.  lon,lat should not be allocated on entry.
-   real(8), intent(out), allocatable, dimension(:) :: lon,lat
-   integer, intent(out) :: n
-   
-   n = 216
-   if (allocated(lon)) deallocate(lon)  ! This isn't necessary
-   if (allocated(lat)) deallocate(lat)
-   allocate(lon(n),lat(n))
-lon(1) = 0.;  lat(1) = 90.
-lon(2) = 0.;  lat(2) = 80.
-lon(3) = 15.;  lat(3) = 80.
-lon(4) = 30.;  lat(4) = 80.
-lon(5) = 45.;  lat(5) = 80.
-lon(6) = 60.;  lat(6) = 80.
-lon(7) = 75.;  lat(7) = 80.
-lon(8) = 90.;  lat(8) = 80.
-lon(9) = 105.;  lat(9) = 80.
-lon(10) = 120.;  lat(10) = 80.
-lon(11) = 135.;  lat(11) = 80.
-lon(12) = 150.;  lat(12) = 80.
-lon(13) = 165.;  lat(13) = 80.
-lon(14) = 180.;  lat(14) = 80.
-lon(15) = 195.;  lat(15) = 80.
-lon(16) = 210.;  lat(16) = 80.
-lon(17) = 225.;  lat(17) = 80.
-lon(18) = 240.;  lat(18) = 80.
-lon(19) = 255.;  lat(19) = 80.
-lon(20) = 270.;  lat(20) = 80.
-lon(21) = 285.;  lat(21) = 80.
-lon(22) = 300.;  lat(22) = 80.
-lon(23) = 315.;  lat(23) = 80.
-lon(24) = 330.;  lat(24) = 80.
-lon(25) = 345.;  lat(25) = 80.
-lon(26) = 0.;  lat(26) = 70.
-lon(27) = 15.;  lat(27) = 70.
-lon(28) = 30.;  lat(28) = 70.
-lon(29) = 45.;  lat(29) = 70.
-lon(30) = 60.;  lat(30) = 70.
-lon(31) = 75.;  lat(31) = 70.
-lon(32) = 90.;  lat(32) = 70.
-lon(33) = 105.;  lat(33) = 70.
-lon(34) = 120.;  lat(34) = 70.
-lon(35) = 135.;  lat(35) = 70.
-lon(36) = 150.;  lat(36) = 70.
-lon(37) = 165.;  lat(37) = 70.
-lon(38) = 180.;  lat(38) = 70.
-lon(39) = 195.;  lat(39) = 70.
-lon(40) = 210.;  lat(40) = 70.
-lon(41) = 225.;  lat(41) = 70.
-lon(42) = 240.;  lat(42) = 70.
-lon(43) = 255.;  lat(43) = 70.
-lon(44) = 270.;  lat(44) = 70.
-lon(45) = 285.;  lat(45) = 70.
-lon(46) = 300.;  lat(46) = 70.
-lon(47) = 315.;  lat(47) = 70.
-lon(48) = 330.;  lat(48) = 70.
-lon(49) = 345.;  lat(49) = 70.
-lon(50) = 0.;  lat(50) = 60.
-lon(51) = 15.;  lat(51) = 60.
-lon(52) = 30.;  lat(52) = 60.
-lon(53) = 45.;  lat(53) = 60.
-lon(54) = 60.;  lat(54) = 60.
-lon(55) = 75.;  lat(55) = 60.
-lon(56) = 90.;  lat(56) = 60.
-lon(57) = 105.;  lat(57) = 60.
-lon(58) = 120.;  lat(58) = 60.
-lon(59) = 135.;  lat(59) = 60.
-lon(60) = 150.;  lat(60) = 60.
-lon(61) = 165.;  lat(61) = 60.
-lon(62) = 180.;  lat(62) = 60.
-lon(63) = 195.;  lat(63) = 60.
-lon(64) = 210.;  lat(64) = 60.
-lon(65) = 225.;  lat(65) = 60.
-lon(66) = 240.;  lat(66) = 60.
-lon(67) = 255.;  lat(67) = 60.
-lon(68) = 270.;  lat(68) = 60.
-lon(69) = 285.;  lat(69) = 60.
-lon(70) = 300.;  lat(70) = 60.
-lon(71) = 315.;  lat(71) = 60.
-lon(72) = 330.;  lat(72) = 60.
-lon(73) = 345.;  lat(73) = 60.
-lon(74) = 0.;  lat(74) = 50.
-lon(75) = 15.;  lat(75) = 50.
-lon(76) = 30.;  lat(76) = 50.
-lon(77) = 45.;  lat(77) = 50.
-lon(78) = 60.;  lat(78) = 50.
-lon(79) = 75.;  lat(79) = 50.
-lon(80) = 90.;  lat(80) = 50.
-lon(81) = 105.;  lat(81) = 50.
-lon(82) = 120.;  lat(82) = 50.
-lon(83) = 135.;  lat(83) = 50.
-lon(84) = 150.;  lat(84) = 50.
-lon(85) = 165.;  lat(85) = 50.
-lon(86) = 180.;  lat(86) = 50.
-lon(87) = 195.;  lat(87) = 50.
-lon(88) = 210.;  lat(88) = 50.
-lon(89) = 225.;  lat(89) = 50.
-lon(90) = 240.;  lat(90) = 50.
-lon(91) = 255.;  lat(91) = 50.
-lon(92) = 270.;  lat(92) = 50.
-lon(93) = 285.;  lat(93) = 50.
-lon(94) = 300.;  lat(94) = 50.
-lon(95) = 315.;  lat(95) = 50.
-lon(96) = 330.;  lat(96) = 50.
-lon(97) = 345.;  lat(97) = 50.
-lon(98) = 0.;  lat(98) = 40.
-lon(99) = 15.;  lat(99) = 40.
-lon(100) = 30.;  lat(100) = 40.
-lon(101) = 45.;  lat(101) = 40.
-lon(102) = 60.;  lat(102) = 40.
-lon(103) = 75.;  lat(103) = 40.
-lon(104) = 90.;  lat(104) = 40.
-lon(105) = 105.;  lat(105) = 40.
-lon(106) = 120.;  lat(106) = 40.
-lon(107) = 135.;  lat(107) = 40.
-lon(108) = 150.;  lat(108) = 40.
-lon(109) = 165.;  lat(109) = 40.
-lon(110) = 180.;  lat(110) = 40.
-lon(111) = 195.;  lat(111) = 40.
-lon(112) = 210.;  lat(112) = 40.
-lon(113) = 225.;  lat(113) = 40.
-lon(114) = 240.;  lat(114) = 40.
-lon(115) = 255.;  lat(115) = 40.
-lon(116) = 270.;  lat(116) = 40.
-lon(117) = 285.;  lat(117) = 40.
-lon(118) = 300.;  lat(118) = 40.
-lon(119) = 315.;  lat(119) = 40.
-lon(120) = 330.;  lat(120) = 40.
-lon(121) = 345.;  lat(121) = 40.
-lon(122) = 0.;  lat(122) = 30.
-lon(123) = 15.;  lat(123) = 30.
-lon(124) = 30.;  lat(124) = 30.
-lon(125) = 45.;  lat(125) = 30.
-lon(126) = 60.;  lat(126) = 30.
-lon(127) = 75.;  lat(127) = 30.
-lon(128) = 90.;  lat(128) = 30.
-lon(129) = 105.;  lat(129) = 30.
-lon(130) = 120.;  lat(130) = 30.
-lon(131) = 135.;  lat(131) = 30.
-lon(132) = 150.;  lat(132) = 30.
-lon(133) = 165.;  lat(133) = 30.
-lon(134) = 180.;  lat(134) = 30.
-lon(135) = 195.;  lat(135) = 30.
-lon(136) = 210.;  lat(136) = 30.
-lon(137) = 225.;  lat(137) = 30.
-lon(138) = 240.;  lat(138) = 30.
-lon(139) = 255.;  lat(139) = 30.
-lon(140) = 270.;  lat(140) = 30.
-lon(141) = 285.;  lat(141) = 30.
-lon(142) = 300.;  lat(142) = 30.
-lon(143) = 315.;  lat(143) = 30.
-lon(144) = 330.;  lat(144) = 30.
-lon(145) = 345.;  lat(145) = 30.
-lon(146) = 0.;  lat(146) = 20.
-lon(147) = 15.;  lat(147) = 20.
-lon(148) = 30.;  lat(148) = 20.
-lon(149) = 45.;  lat(149) = 20.
-lon(150) = 60.;  lat(150) = 20.
-lon(151) = 75.;  lat(151) = 20.
-lon(152) = 90.;  lat(152) = 20.
-lon(153) = 105.;  lat(153) = 20.
-lon(154) = 120.;  lat(154) = 20.
-lon(155) = 135.;  lat(155) = 20.
-lon(156) = 150.;  lat(156) = 20.
-lon(157) = 165.;  lat(157) = 20.
-lon(158) = 180.;  lat(158) = 20.
-lon(159) = 195.;  lat(159) = 20.
-lon(160) = 210.;  lat(160) = 20.
-lon(161) = 225.;  lat(161) = 20.
-lon(162) = 240.;  lat(162) = 20.
-lon(163) = 255.;  lat(163) = 20.
-lon(164) = 270.;  lat(164) = 20.
-lon(165) = 285.;  lat(165) = 20.
-lon(166) = 300.;  lat(166) = 20.
-lon(167) = 315.;  lat(167) = 20.
-lon(168) = 330.;  lat(168) = 20.
-lon(169) = 345.;  lat(169) = 20.
-lon(170) = 0.;  lat(170) = 10.
-lon(171) = 15.;  lat(171) = 10.
-lon(172) = 30.;  lat(172) = 10.
-lon(173) = 45.;  lat(173) = 10.
-lon(174) = 60.;  lat(174) = 10.
-lon(175) = 75.;  lat(175) = 10.
-lon(176) = 90.;  lat(176) = 10.
-lon(177) = 105.;  lat(177) = 10.
-lon(178) = 120.;  lat(178) = 10.
-lon(179) = 135.;  lat(179) = 10.
-lon(180) = 150.;  lat(180) = 10.
-lon(181) = 165.;  lat(181) = 10.
-lon(182) = 180.;  lat(182) = 10.
-lon(183) = 195.;  lat(183) = 10.
-lon(184) = 210.;  lat(184) = 10.
-lon(185) = 225.;  lat(185) = 10.
-lon(186) = 240.;  lat(186) = 10.
-lon(187) = 255.;  lat(187) = 10.
-lon(188) = 270.;  lat(188) = 10.
-lon(189) = 285.;  lat(189) = 10.
-lon(190) = 300.;  lat(190) = 10.
-lon(191) = 315.;  lat(191) = 10.
-lon(192) = 330.;  lat(192) = 10.
-lon(193) = 345.;  lat(193) = 10.
-lon(194) = 0.;  lat(194) = 0.
-lon(195) = 15.;  lat(195) = 0.
-lon(196) = 30.;  lat(196) = 0.
-lon(197) = 45.;  lat(197) = 0.
-lon(198) = 60.;  lat(198) = 0.
-lon(199) = 75.;  lat(199) = 0.
-lon(200) = 90.;  lat(200) = 0.
-lon(201) = 105.;  lat(201) = 0.
-lon(202) = 120.;  lat(202) = 0.
-lon(203) = 135.;  lat(203) = 0.
-lon(204) = 150.;  lat(204) = 0.
-lon(205) = 165.;  lat(205) = 0.
-lon(206) = 180.;  lat(206) = 0.
-lon(207) = 195.;  lat(207) = 0.
-lon(208) = 210.;  lat(208) = 0.
-lon(209) = 225.;  lat(209) = 0.
-lon(210) = 240.;  lat(210) = 0.
-lon(211) = 255.;  lat(211) = 0.
-lon(212) = 270.;  lat(212) = 0.
-lon(213) = 285.;  lat(213) = 0.
-lon(214) = 300.;  lat(214) = 0.
-lon(215) = 315.;  lat(215) = 0.
-lon(216) = 330.;  lat(216) = 0.
-lon(217) = 345.;  lat(217) = 0.
-
-   
-   end subroutine sphere_sample_internal
 !-------------------------------------------------------------------------------
---
+++ CIJ_plot/src/CIJ_sphere.bash
@@ -0,0 +1,286 @@
+#!/bin/bash
+# CIJ_sphere.bash
+# Shell script to plot phase velocity surfaces for elastic constants on a sphere
+# with the viewpoint of one's choosing
+
+##########################################
+# Set path to CIJ_plot_cij2phasevels here:
+BINDIR=~nowacki/Applications/Elasticity/CIJ_plot/src
+##########################################
+
+usage () {
+	{
+	echo "`basename $0`: Plots phase velocity surfaces for sets of elastic constants."
+	echo "Usage: `basename $0` (-r density (.ecs file))"
+	echo "Options:"
+	echo "  -P|-S                   : Show P wave velocity or S wave anisotropy sphere. (S)"
+	echo "  -a [azi inc]            : Set viepoint: azimuth is in CIJ_phasevels"
+	echo "                            convention (az x1->x2; inc x1x2->x3) (45 30)"
+	echo "  -b(atch)                : Don't display plot for batch plotting. (display)"
+	echo "  -c(pt) [colour palette] : Choose alternative colour palette."
+	echo "                            Must be inbuilt to GMT. (wysiwyg)"
+	echo "  -h(elp)                 : Print this message."
+	echo "  -d [azi inc label]      : Mark a point on the sphere with a label."
+	echo "  -n [no. contours]       : Number of contours (10)"
+	echo "  -noaxes                 : Don't plot principal axes. (plot)"
+	echo "  -norm(alise)            : Input ecs require density normalisation."
+	echo "  -o [outfile]            : Send output to output file. (temporary file)"
+	echo "  -s(cale) [(vp1 vp2)|(avs1 avs2)] : Set limits on colour scale.  (automatic)"
+	# echo "  -t(itle) [title string] : Set title for plot, including GMT codes. (none)"
+	echo "  Density can be determined from .ecs file or specified.  Must be determined"
+	echo "  for list input either on command line with -r, or as last value on stdin."
+	} > /dev/stderr
+	exit 1
+}
+
+# Wrapper function to make a temporary file with the desired suffix and exit the
+# script if we can't do so.
+make_temp_file () {
+	[ $# -ne 1 ] && echo "`basename $0`: make_temp_file: Require name of temp file" &&
+		exit 2
+	local f=$(mktemp /tmp/CIJ_plot."$1"XXXXXX) ||
+		{ echo "`basename $0`: make_temp_file: Can't create file $f"; exit 3; }
+	echo $f
+}
+
+WIDTH=5
+vlength=0.3 # / cm
+
+# Defaults
+lower=0  # Upper hemisphere
+scale=0  # Automatic scale
+list=1   # Read from stdin
+PROJ=G   # Projection always G
+nlevels=10 # Numver of contours
+cmap=wysiwyg # Colour palette
+S=1      # S wave anisotropy sphere
+inc=30   # View incidence up from x1-x2 plane towards x3
+azi=45   # View azimuth from x1 towards x2
+nd=0     # No additional directions plotted
+
+# Get options and density
+while [ -n "$1" ]; do
+	case "$1" in
+		-P)
+			p=1
+			shift
+			;;
+		-S)
+			unset p
+			shift
+			;;
+		-a)
+			azi="$2"
+			inc="$3"
+			shift 3
+			;;
+		-b|-batch)
+			batch=1
+			shift
+			;;
+		-c|-cpt)
+			cmap=$2
+			shift 2
+			;;
+		-d)
+			((nd++))
+			dazi[nd]="$2"
+			dinc[nd]="$3"
+			dlabel[nd]="$4"
+			shift 4
+			;;
+		-s|-scale)
+			scale=1
+			vp1=$2
+			vp2=$3
+			avs1=$2
+			avs2=$3
+			shift 3
+			;;
+		-t|-title)
+			title="$2"
+			shift 2
+			;;
+		-o)
+			FIG="$2"
+			OUTPUT=1
+			shift 2
+			;;
+		-r)
+			rho=$2
+			shift 2
+			;;
+		-noaxes)
+			noaxes=1
+			shift
+			;;
+		-n|-nlevels)
+			nlevels=$2
+			shift 2
+			;;
+		-norm|-normalise)
+			normalise=1
+			shift
+			;;
+		*)
+			list=0
+			file=$1
+			if ! [ -r "$1" ]; then
+				echo "`basename $0`: unrecognised option or can't read file: $1" > /dev/stderr
+				usage
+				exit 2
+			fi
+			shift
+			;;
+	esac
+done
+
+# Set FIG to a temp file if not otherwise set
+[ -z "$FIG" ] && FIG=`make_temp_file ps`
+
+# Don't flip the seismic colourmap
+flip="-I"
+if [[ "$cmap" == "seis" ]]; then
+	flip=""
+fi
+
+# Produce values for phase velocity surface
+if [ $list -eq 0 ]; then  # Single .ecs file entry
+	ecs=`ecs2ij $file`
+else                      # Single set of ecs from stdin
+	ecs=`cat /dev/stdin | head -n1`
+fi
+
+# If we have 37 numbers on stdin, the last is density.  Override this with -r option.
+if [ $list -eq 1 ]; then
+	necs=`echo $ecs | wc -w`
+	if [ $necs -eq 37 -a -z "$rho" ]; then
+		rho=`echo $ecs | awk '{print $NF}'`
+		ecs=`echo $ecs | awk '{$NF=""; print}'`
+	elif [ $necs -eq 36 -a -z "$rho" ]; then
+		echo "`basename $0`: rho must be found on stdin (last value), in .ecs file, or with -r [rho]." > /dev/stderr
+		exit 5
+	fi
+fi
+
+# If no r supplied and we aren't reading from a .ecs file, stop
+if [ $list -eq 1 -a -z "$rho" ]; then
+	echo "`basename $0`: must either use rho of .ecs file or specify if reading ecs from stdin." > /dev/stderr
+	exit 5
+elif [ $list -eq 0 -a -z "$rho" ]; then   # Taking rho from .ecs file
+	rho=`awk '$1==7&&$2==7{print $3}' $file`
+	echo "Density: $rho kg/m^3"
+fi
+
+# If required, normalise by density
+if [ -n "$normalise" ]; then
+	ecs=`echo $ecs | awk -v r=$rho '{for (i=1;i<=NF;i++) printf("%s ",$i*r)}'`
+fi
+
+# Get projection
+azi=$(echo "-1*$azi" | bc -l)
+PROJ=${PROJ}${azi}/${inc}/${WIDTH}c
+
+# Set up common temp files
+CPT=`make_temp_file cpt`
+GRD=`make_temp_file grd`
+  S=`make_temp_file S`
+  F=`make_temp_file F`
+  P=`make_temp_file P`
+trap "rm -f $P $S $F $CPT $GRD" EXIT
+
+# Start of postscript
+psxy -J${PROJ} -Rd -K -T -P > $FIG
+
+########################################
+# P wave velocity plot
+if [ $p ]; then
+
+	echo "$ecs $rho" | ${BINDIR}/CIJ_plot_cij2phasevels PS > $P
+
+	min=`tail -n1 $P | awk '{printf("%0.2e", $2*0.99)}'`
+	max=`tail -n1 $P | awk '{printf("%0.2e", $3*1.01)}'`
+	AVp=`tail -n1 $P | awk '{printf("%0.1f", 200*($3-$2)/($2+$3) )}'`
+	if [ $scale -eq 1 ]; then # Have defined scale
+		d=`echo $vp2 $vp1 $nlevels | awk '{printf("%0.1e",($1-$2)/$3)}'`
+		makecpt -C${cmap} -T$vp1/$vp2/$d $flip > $CPT
+	else                     # Automatic scale
+		d=`echo $max $min $nlevels | awk '{printf("%0.1e",($1-$2)/$3)}'`
+		makecpt -C${cmap} -T$min/$max/$d $flip > $CPT
+	fi
+	grep -v ">" $P | surface -G$GRD -Rd -I1
+
+	grdimage $GRD -J${PROJ} -Rd -C$CPT -Bnsew -O -K >> "$FIG" 2>/dev/null
+	psscale -C$CPT -D`echo $WIDTH*1.05 | bc -l`c/`echo $WIDTH/2 | bc -l`c/${WIDTH}c/0.3c \
+		-B/:"@%2%V@%%@-P@- / km s@+-1": -O -K >> "$FIG"
+	
+
+#######################################
+# S wave anisotropy plot
+else
+	
+	echo "$ecs $rho" | ${BINDIR}/CIJ_plot_cij2phasevels SS > $S
+	echo "$ecs $rho" | ${BINDIR}/CIJ_plot_cij2phasevels FS > $F
+	min=`tail -n1 $S | awk '{printf("%0.2e", $2*0.95)}'`
+	max=`tail -n1 $S | awk '{printf("%0.2e", $3*1.05)}'`
+	if [ $scale -eq 1 ]; then
+		d=`echo $avs2 $avs1 $nlevels | awk '{printf("%0.1e",($1-$2)/$3)}'` #`echo "($max-$min)/10" | bc -l`
+		makecpt -C${cmap} -T$avs1/$avs2/$d $flip > $CPT
+	else
+		d=`echo $max $min $nlevels | awk '{printf("%0.1e",($1-$2)/$3)}'` #`echo "($max-$min)/10" | bc -l`
+		makecpt -C${cmap} -T$min/$max/$d $flip > $CPT
+	fi
+
+	grep -v ">" $S | surface -G$GRD -Rd -I1
+
+	grdimage $GRD -J${PROJ} -Rd -C$CPT -Bnsew -O -K >> "$FIG" 2>/dev/null
+	psscale -C$CPT -D`echo $WIDTH*1.05 | bc -l`c/`echo $WIDTH/2 | bc -l`c/${WIDTH}c/0.3c \
+		-B/:"A@%2%V@%%@-S@- / %": -O -K >> "$FIG"
+	# echo "0 0 12 0 0 CM A@%2%V@%%@-S@- / %" |\
+	# 	pstext -J -R -N -O -K -D0/`echo 0.15*$WIDTH | bc -l`c >> "$FIG"
+
+	# S fast orientation plot
+	awk -v s=$vlength 'NF==3{print $0,s "c"}' $F |\
+		psxy -J -R -SVB0.08c/0/0 -Gblack -W0.005c,white -O -K >> "$FIG"
+	
+fi
+
+####################################
+# Principal axes if desired
+if [ -z "$noaxes" ]; then
+	dirs=`awk -v azi=$azi -v inc=$inc '
+		function abs(x) {return (x>0) ? x : -x}
+		# Plot either + or - each axis depending on what view we are using
+		BEGIN {
+			if (inc >= 0) print "  0  90 10 0 0 CB  @%2%x@%%@-3"
+			if (inc <  0) print "  0 -90 10 0 0 CB -@%2%x@%%@-3"
+			
+			azi = (azi+3600+180)%360 - 180  # In range -180 to 180
+			if (azi >  0) print " 90   0 10 0 0 CB -@%2%x@%%@-2"
+			if (azi <= 0) print "-90   0 10 0 0 CB  @%2%x@%%@-2"
+			if (abs(azi) <= 90) print "  0   0 10 0 0 CB  @%2%x@%%@-1"
+			if (abs(azi) >  90) print "  0 180 10 0 0 CB -@%2%x@%%@-1"
+		}'`
+	echo "$dirs" |
+	psxy -J -R -O -K -Ss0.2c -Gwhite -W0.5p -N >> "$FIG"
+	echo "$dirs" |
+	pstext -J -R -O -K -D0/0.25c -Wwhite -N >> "$FIG"
+fi
+
+####################################
+# Additional directional labels
+for ((i=1; i<=nd; i++)); do
+	echo $(echo "-1*${dazi[i]}" | bc -l) ${dinc[i]} |
+		psxy -J -R -O -K -Sc0.3c -Gyellow -W0.5p -N >> "$FIG"
+	echo $(echo "-1*${dazi[i]}" | bc -l) ${dinc[i]} 11 0 0 CB "${dlabel[i]}"|
+		pstext -J -R -O -K -D0/0.25c -Wwhite -N >> "$FIG"
+done
+
+# Finalise Postscript
+psxy -J -R -O -T >> "$FIG"
+
+[ -z "$batch" ] && gv "$FIG" 2>/dev/null
+
+[ -z $OUTPUT ] && /bin/rm -f "$FIG"
+
+exit 0
